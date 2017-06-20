78ce5b4b5dec780edcb1274409a99d43700bd937
nleva <n.lev.a@ya.ru>
2017-06-20 19:53:49 +0300

    package fix

--- GwtArchetype/src/main/resources/archetype-resources/src/main/java/client/__module__.java
+++ GwtArchetype/src/main/resources/archetype-resources/src/main/java/client/__module__.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package org.web.app.client;
+package ${package}.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
